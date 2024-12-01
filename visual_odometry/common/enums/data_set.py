ce8f7c2b945e1db0a83ca19439c65ee6abc86cdd
flochkristof <kristof.floch@gmail.com>
2024-12-01 12:10:28 +0100

    cleanup with baseclass logging added dataset enum

---
+++ visual_odometry/common/enums/data_set.py
@@ -0,0 +1,6 @@
+from enum import IntEnum
+
+class DataSet(IntEnum):
+    KITTI = 0
+    MALAGA  = 1
+    PARKING = 2
--- visual_odometry/main.py
+++ visual_odometry/main.py
@@ -1,26 +1,39 @@
 import numpy as np
+import os
+
+from visual_odometry.common import ParamServer
+from visual_odometry.common import BaseClass
+from visual_odometry.common.enums import LogLevel
+from visual_odometry.common.enums import DataSet
 
 
-class VisualOdometryPipeline:
+class VisualOdometryPipeline(BaseClass):
+    def __init__(self, param_server: ParamServer, debug: LogLevel = LogLevel.INFO):
+        super().__init__(debug)
+        self._param_server = param_server
 
-    def run(self, ds: int = 0):
-        print("Driver Method for Visual Odometry Pipeline")
-        kitti_path = '../datasets/kitti'
-        malaga_path = '../datasets/malaga-urban-dataset-extract-07'
-        parking_path = '../datasets/parking'
+        self._dataset_paths = {"KITTI": os.path.join(os.path.dirname(os.path.dirname(__file__)), "datasets/kitti"),
+                               "MALAGA": os.path.join(os.path.dirname(os.path.dirname(__file__)), "datasets/malaga-urban-dataset-extract-07"),
+                               "PARKING": os.path.join(os.path.dirname(os.path.dirname(__file__)), "datasets/parking")}
 
+        self._info_print(f"Keypoint tracker initialized\n  -Log level: {debug.name}\n - ParamServer: {self._param_server}")
+        
+
+
+
+    def run(self, dataset: DataSet = DataSet.KITTI):
+        self._info_print(f"Running pipeline for dataset: {dataset.name}")
         # Setup
-        if ds == 0:
-            print("Using KITTI dataset")
-            ground_truth = np.loadtxt(kitti_path + '/poses/05.txt')
+        if dataset == DataSet.KITTI:
+            ground_truth = np.loadtxt(self._dataset_paths["KITTI"] + '/poses/05.txt')
             ground_truth = ground_truth[:, [-9, -1]]
             last_frame = 4540
             K = np.array([[7.188560000000e+02, 0, 6.071928000000e+02],
                          [0, 7.188560000000e+02, 1.852157000000e+02],
                          [0, 0, 1]])
-        elif ds == 1:
+        elif dataset == 1:
             pass
-        elif ds == 2:
+        elif dataset == 2:
             pass
         else:
             assert (False)
